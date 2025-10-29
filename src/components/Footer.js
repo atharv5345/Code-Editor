dd4154db6650eb66577146793929cf213aae77d2
Atharv Bhalerao <bhaleraoatharv888@gmail.com>
2025-10-29 14:53:20 +0530

    Footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
     <div>
       <Container fluid className="footer">
         Copywrite © {year} | Made with <i className="far fa-heart"></i>{" "}
-        Sahil Borse
+        Atharv Bhalerao
       </Container>
     </div>
   );
